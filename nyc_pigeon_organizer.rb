bcf568deea8c27c90531dc652ec9750d46525a18
classy-tier-0049 <mauricelynell@gmail.com>
2020-09-02 22:19:12 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,7 +2,8 @@ require 'pry'
 def nyc_pigeon_organizer(data)
   data.each_with_object({}) do |(key, value), fin_array|
     #binding.pry
-    value.each do |
+    value.each do |next_key, next_value|
+      binding.pry
     fin_array
   end
 end

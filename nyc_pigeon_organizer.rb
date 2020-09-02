01cfad79a8258bee90f4d39ad5a2c664e06ecc22
classy-tier-0049 <mauricelynell@gmail.com>
2020-09-02 22:59:43 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,7 +3,7 @@ def nyc_pigeon_organizer(data)
   data.each_with_object({}) do |(key, value), fin_array|
     value.each do |next_key, next_value|
       next_value.each do |name|
-        if !fin_array.has_key? '${name}'
+        if !fin_array.has_key? name
           fin_array[name] = {}
         end
         if !fin_array[name][key]

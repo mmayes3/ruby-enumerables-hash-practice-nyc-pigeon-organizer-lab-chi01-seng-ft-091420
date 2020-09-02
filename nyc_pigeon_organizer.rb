554c4205f00f7346a4ecb9c7fbd120f0ffd19a25
classy-tier-0049 <mauricelynell@gmail.com>
2020-09-02 22:28:35 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,8 +5,8 @@ def nyc_pigeon_organizer(data)
     value.each do |next_key, next_value|
       next_value.each do |name|
         if name not in fin_array
-        binding.pry
           fin_array[name] = {}
+       binding.pry 
         end
           
       end

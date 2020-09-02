86261e2657fa2945e568fc8c695945a006a42f5a
classy-tier-0049 <mauricelynell@gmail.com>
2020-09-02 22:34:43 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,9 +5,9 @@ def nyc_pigeon_organizer(data)
     value.each do |next_key, next_value|
       next_value.each do |name|
         if name !fin_array[name]
-  
           fin_array[name] = {}
         end
+        
       end
     end
     
